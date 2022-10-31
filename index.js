64fce53122d65c4784e47137b954480c731dd3f3
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2022-10-31 15:49:10 +0330

    special

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-let times = ["10-8","12-10","14-12","18-16"]
-let day = ["شنبه","یکشنبه","دوشنبه","سه شنبه","چهارشنبه","پنجشنبه","جمعه"]
+let Plan = [];
+// let times = ["10-8","12-10","14-12","18-16"];
+let day = ["شنبه","یکشنبه","دوشنبه","سه شنبه","چهارشنبه","پنجشنبه","جمعه"];
 let id = times.length * day.length;
-
-let hoursHtml = `<thead><tr><th></th>`
-for (let m = 0; m < times.length; m++) {
-    hoursHtml += `<th>${times[m]}</th>`
-    
-}
-document.getElementsByTagName("table")[0].innerHTML +=hoursHtml+`</tr></thead><tbody>`;
-
-
-let string ="";
-for (let i = 0; i < day.length; i++) {
-    string+=`<tr><td>${day[i]}</td>`
-   
-
-    for (let j = 0; j < times.length; j++) {
-        string += `<td class="select"><button class="button" id=${id--} onclick=changeGender(${id + 1})>آقایان</button></td>`
+function createColumns()  {
+    document.getElementById("table").innerHTML = "";
+    let input = document.getElementById("1").value;
+    let hoursHtml = `<thead><tr><th></th>`
+    for (let m = 0; m < input; m++) {
+        hoursHtml += `<th>${m+1}</th>`
         
     }
+    document.getElementsByTagName("table")[0].innerHTML +=hoursHtml+`</tr></thead><tbody>`;
+    
+    let string ="";
+    for (let i = 0; i < day.length; i++) {
+        string+=`<tr><td>${day[i]}</td>`;
+       
+    let arr = [];
+        for (let j = 0; j < input; j++) {
+            arr.push(id);
+            string += `<td class="select"><button class="button" id=${id--} onclick=changeGender(${id + 1})>بانوان</button></td>`;
+            
+            
+        }
+        Plan.push(arr)
+    }
+    
+    document.getElementsByTagName("table")[0].innerHTML +=string+`<tr></tbody>`;
+    
 }
-
-document.getElementsByTagName("table")[0].innerHTML +=string+`<tr></tbody>`;
 
 
 function changeGender(id){
@@ -34,4 +41,24 @@ function changeGender(id){
     else
         document.getElementById(id).innerHTML = "آقایان";
     
+}
+
+function GetData(){
+    let ExportData = [];
+    for(i = 0; i < Plan.length; i++) {
+        ExportData.push([]); 
+        for(j = 0; j < Plan[i].length; j++){
+            let value = document.getElementById(Plan[i][j]).innerHTML;
+            if(value == "آقایان") {
+                 ExportData[i][j] = 0;
+            } else if (value == "بانوان") {
+                ExportData[i][j] = 1;
+            } else {
+                ExportData[i][j] = 2;
+            }
+            
+        }
+    }
+
+    alert( JSON.stringify(ExportData) )
 }
